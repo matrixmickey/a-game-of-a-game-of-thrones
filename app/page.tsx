e20d05e0551dcc39c5d4d68f2d60419e12048b32
matrixmickey <matrixmickey@gmail.com>
2026-05-06 17:23:05 -0400

    add authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import { auth0 } from "@/lib/auth0";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession();
+
   return (
+    <>
     <Image
       src="/images/board.jpg"
       alt="The game board should be displaying here..."
       width={1980}
       height={2975}
     />
+    <div>
+      {session ? `Welcome ${session.user.name}` : "Log in to play"}
+    </div>
+    </>
   );
 }
