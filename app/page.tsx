a2b4badbef80f0a838bd0bdd5f8662d991fd2ab0
matrixmickey <matrixmickey@gmail.com>
2026-05-12 15:47:33 -0400

    join game

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,70 @@ import Area from "@/components/Area";
 import HousePieceImage from "@/components/HousePieceImage";
 import { auth0 } from "@/lib/auth0";
 import { HousePiece } from "@/types/HousePiece";
+import { Player } from "@/types/Player";
 import { BigQuery } from "@google-cloud/bigquery";
+import { revalidatePath } from "next/cache";
 import Image from "next/image";
 
 export default async function Home() {
+  async function joinGame() {
+    "use server";
+
+    await new BigQuery().query(`
+      INSERT INTO \`a-game-of-a-game-of-thrones.dataset.players\` (email, house)
+      WITH PossibleValues AS (
+        SELECT ['Baratheon', 'Lannister', 'Stark'] AS options
+      ),
+      AvailableValues AS (
+        SELECT val
+        FROM PossibleValues,
+        UNNEST(options) AS val
+        WHERE val NOT IN (SELECT DISTINCT house FROM \`a-game-of-a-game-of-thrones.dataset.players\`)
+      )
+      SELECT '${(await auth0.getSession())?.user.email}' AS email, val AS house
+      FROM AvailableValues
+      ORDER BY RAND() -- Randomize order
+      LIMIT 1;
+    `);
+    
+    revalidatePath('/');
+  }
+
   const session = await auth0.getSession();
 
   const bigquery = new BigQuery();
 
-  let [rows] = await bigquery.query('SELECT * FROM `a-game-of-a-game-of-thrones.dataset.house-pieces` LIMIT 1000');
+  const [playerRows] = await bigquery.query('SELECT * FROM `a-game-of-a-game-of-thrones.dataset.players` LIMIT 1000');
 
-  if (rows.length === 0) {
+  const players = playerRows as Player[];
+
+  const player = session ? players.find(player => player.email === session?.user.email) : undefined;
+
+  if (players.length < 6) {
+    if (!session) {
+      return "Log in to play";
+    } else if (!player) {
+      return (
+        <form action={joinGame}>
+          <button type="submit">Join Game</button>
+        </form>
+      )
+    }
+  }
+
+  let [housePieceRows] = await bigquery.query('SELECT * FROM `a-game-of-a-game-of-thrones.dataset.house-pieces` LIMIT 1000');
+
+  if (housePieceRows.length === 0) {
     await bigquery.query(`INSERT INTO \`a-game-of-a-game-of-thrones.dataset.house-pieces\` (house, type, area) VALUES
       ('Lannister', 'footman', 'Stoney Sept'),
       ('Lannister', 'footman', 'Lannisport'),
       ('Lannister', 'knight', 'Lannisport'),
       ('Lannister', 'ship', 'The Golden Sound');
     `);
-    [rows] = await bigquery.query('SELECT * FROM `a-game-of-a-game-of-thrones.dataset.house-pieces` LIMIT 1000');
+    [housePieceRows] = await bigquery.query('SELECT * FROM `a-game-of-a-game-of-thrones.dataset.house-pieces` LIMIT 1000');
   }
 
-  const housePieces = rows as HousePiece[];
+  const housePieces = housePieceRows as HousePiece[];
 
   return (
     <>
@@ -40,27 +83,28 @@ export default async function Home() {
       <Area top={54} left={17} width={7} height={10} housePieces={housePieces.filter(housePiece => housePiece.area === "Lannisport")} />
       <Area top={58} left={25} width={11} height={6} housePieces={housePieces.filter(housePiece => housePiece.area === "Stoney Sept")} />
     </div>
-    <div>
-      {session ? 
+    {player &&
       <>
-      <HousePieceImage id="raid-special" src="/images/house-pieces/order-tokens/RaidSpecial.png" />
-      <HousePieceImage id="raid-1" src="/images/house-pieces/order-tokens/Raid.png" />
-      <HousePieceImage id="raid-2" src="/images/house-pieces/order-tokens/Raid.png" />
-      <HousePieceImage id="march-special" src="/images/house-pieces/order-tokens/MarchSpecial.png" />
-      <HousePieceImage id="march" src="/images/house-pieces/order-tokens/March.png" />
-      <HousePieceImage id="march-minus-one" src="/images/house-pieces/order-tokens/MarchMinusOne.png" />
-      <HousePieceImage id="defense-special" src="/images/house-pieces/order-tokens/DefenseSpecial.png" />
-      <HousePieceImage id="defense-1" src="/images/house-pieces/order-tokens/Defense.png" />
-      <HousePieceImage id="defense-2" src="/images/house-pieces/order-tokens/Defense.png" />
-      <HousePieceImage id="support-special" src="/images/house-pieces/order-tokens/SupportSpecial.png" />
-      <HousePieceImage id="support-1" src="/images/house-pieces/order-tokens/Support.png" />
-      <HousePieceImage id="support-2" src="/images/house-pieces/order-tokens/Support.png" />
-      <HousePieceImage id="consolidate-power-special" src="/images/house-pieces/order-tokens/ConsolidatePowerSpecial.png" />
-      <HousePieceImage id="consolidate-power-1" src="/images/house-pieces/order-tokens/ConsolidatePower.png" />
-      <HousePieceImage id="consolidate-power-2" src="/images/house-pieces/order-tokens/ConsolidatePower.png" />
+      <div>You are House {player.house}</div>
+      <div>
+        <HousePieceImage id="raid-special" src="/images/house-pieces/order-tokens/RaidSpecial.png" />
+        <HousePieceImage id="raid-1" src="/images/house-pieces/order-tokens/Raid.png" />
+        <HousePieceImage id="raid-2" src="/images/house-pieces/order-tokens/Raid.png" />
+        <HousePieceImage id="march-special" src="/images/house-pieces/order-tokens/MarchSpecial.png" />
+        <HousePieceImage id="march" src="/images/house-pieces/order-tokens/March.png" />
+        <HousePieceImage id="march-minus-one" src="/images/house-pieces/order-tokens/MarchMinusOne.png" />
+        <HousePieceImage id="defense-special" src="/images/house-pieces/order-tokens/DefenseSpecial.png" />
+        <HousePieceImage id="defense-1" src="/images/house-pieces/order-tokens/Defense.png" />
+        <HousePieceImage id="defense-2" src="/images/house-pieces/order-tokens/Defense.png" />
+        <HousePieceImage id="support-special" src="/images/house-pieces/order-tokens/SupportSpecial.png" />
+        <HousePieceImage id="support-1" src="/images/house-pieces/order-tokens/Support.png" />
+        <HousePieceImage id="support-2" src="/images/house-pieces/order-tokens/Support.png" />
+        <HousePieceImage id="consolidate-power-special" src="/images/house-pieces/order-tokens/ConsolidatePowerSpecial.png" />
+        <HousePieceImage id="consolidate-power-1" src="/images/house-pieces/order-tokens/ConsolidatePower.png" />
+        <HousePieceImage id="consolidate-power-2" src="/images/house-pieces/order-tokens/ConsolidatePower.png" />
+      </div>
       </>
-      : "Log in to play"}
-    </div>
+    }
     </>
   );
 }
---
+++ types/Player.ts
@@ -0,0 +1,4 @@
+export type Player = {
+    email: string;
+    house: string;
+}
