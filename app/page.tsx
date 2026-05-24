e926292ddb4ac2b82abd67f3d52ef93c4713afdf
matrixmickey <matrixmickey@gmail.com>
2026-05-24 11:07:49 -0400

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default async function Home() {
         <div>Nothing right now. Waiting on Houses: {players.filter(player => !player.isDone).map(player => <div>player.house</div>)}</div>
       : game.phase === 'Planning - Assign Orders' &&
         <>
-        <div>Place exactly one Order token on each area you control that contains at least one of youe units (Footman, Knight, Ship, or Siege Engine). The other players will not see which Order token you have assigned to each area until after all players have submitted their assignments. These are your Order tokens:</div>
+        <div>Place exactly one Order token on each area you control that contains at least one of your units (Footman, Knight, Ship, or Siege Engine). The other players will not see which Order token you have assigned to each area until after all players have submitted their assignments. These are your Order tokens:</div>
         </>
       }
       </>
