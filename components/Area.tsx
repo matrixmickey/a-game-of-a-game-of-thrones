606cb7905a1e07209705b8dab178e46447db32f0
matrixmickey <matrixmickey@gmail.com>
2026-06-03 16:08:02 -0400

    Planning - Reveal Orders

--- components/Area.tsx
+++ components/Area.tsx
@@ -24,11 +24,11 @@ export default function Area({name, top, left, width, height, phase, you, pieces
             } : undefined}
             onClick={containsYourUnits ? movePieceHere : undefined}
         >
-            {pieces.filter(piece => piece.area !== name ? false : piece.type !== "order" ? true : piece.house !== you?.house ? false : you.isDone).map((unmovablePiece, index) => {
+            {pieces.filter(piece => piece.area !== name ? false : piece.type !== "order" ? true : phase !== "Planning - Assign Orders" ? true : piece.house !== you?.house ? false : you.isDone).map((unmovablePiece, index) => {
                 const id = `house-piece-top-${top}-left-${left}-index-${index}`;
                 return <PieceComponent key={index} src={`/images/pieces/${unmovablePiece.type}/${unmovablePiece.name}${unmovablePiece.type !== "order" ? `/${unmovablePiece.house}` : ""}.png`} alt={id} />
             })}
-            {pieces.filter(piece => piece.area !== name ? false : piece.house !== you?.house ? false : you.isDone ? false : piece.type === "order").map((movablePiece, index) => {
+            {pieces.filter(piece => piece.area !== name ? false : phase !== "Planning - Assign Orders" ? false : piece.house !== you?.house ? false : you.isDone ? false : piece.type === "order").map((movablePiece, index) => {
                 return <MovablePiece key={index} pieceComponent={<PieceComponent src={`/images/pieces/${movablePiece.type}/${movablePiece.name}${movablePiece.type !== "order" ? `/${movablePiece.house}` : ""}.png`} alt={movablePiece.name} />} thisPiece={movablePiece} pieces={pieces} setPieces={setPieces} />
             })}
         </div>
